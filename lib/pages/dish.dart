5498a2ca50420abfd1fbaa4a54d1f3e278383106
Guilherme Galabarof <gui.guialbano@gmail.com>
2025-11-24 02:59:56 -0300

    Add DishFormPage with app bar and back navigation

--- lib/pages/dish.dart
+++ lib/pages/dish.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DishFormPage extends StatefulWidget {
   const DishFormPage({super.key});
@@ -11,6 +12,13 @@ class _DishFormPage extends State<DishFormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(child: const Text('New Dish')),
+        leading: IconButton(
+          onPressed: () => context.pop(),
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
       body: Center(child: Container(child: Text('data'))),
     );
   }
